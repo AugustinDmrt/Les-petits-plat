22ae4e4267e261e806fad74960143302a9092f24
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2023-09-21 16:41:39 +0200

    Filter
    Filtre sur la barre de recherche + les 3 select

--- scripts/filterData.js
+++ scripts/filterData.js
@@ -4,8 +4,77 @@ let ustensilesFilter = document.getElementById('ustensilesFilter');
 let searchBar = document.getElementById('searchBar');
 let recipeCard = document.getElementById('recipList');
 
-
-
-
 searchBar.addEventListener('keyup', searchRecipes);
+ingredientFilter.addEventListener('change', ingredientSelector);
+appareilsFilter.addEventListener('change', appareilsSelector);
+ustensilesFilter.addEventListener('change', ustensilesSelector);
+
+function searchRecipes(e){
+    let searchTerm = e.target.value.toLowerCase();
+    const recipes = recipeCard.childNodes;
+    recipes.forEach(function(recipe){
+        if(recipe.className === 'card'){
+            let recipeName = recipe.childNodes[1].childNodes[0].innerHTML.toLowerCase();
+            if(recipeName.includes(searchTerm)){
+                recipe.style.display = 'flex';
+            }else{
+                recipe.style.display = 'none';
+            }
+
+        }
+    });
+}
+
+function ingredientSelector(e){
+    let ingredientValue = e.target.value.toLowerCase();
+    const recipes = recipeCard.childNodes;
+    recipes.forEach(function(recipe){
+        if(recipe.className === 'card'){
+            let ingredients = recipe.childNodes[3].childNodes[1].childNodes;
+            ingredients.forEach(function(ingredient){
+                let ingredientName = ingredient.childNodes[0].innerHTML.toLowerCase();
+                if(ingredientName.toLowerCase().includes(ingredientValue)){
+                    recipe.style.display = 'flex';
+                }else{
+                    recipe.style.display = 'none';
+                }
+            });
+        }
+    });
+}
+
+
+function appareilsSelector(e){
+    let appareilValue = e.target.value.toLowerCase();
+    const recipes = recipeCard.childNodes;
+    recipes.forEach(function(recipe){
+        if(recipe.className === 'card'){
+            let appareils = recipe.childNodes[2].childNodes[2].value.toLowerCase();
+            if(appareils.includes(appareilValue)){
+                recipe.style.display = 'flex';
+            }else{
+                recipe.style.display = 'none';
+            }
+        }
+    });
+}
+            
+
+function ustensilesSelector(e){
+    let ustensileValue = e.target.value.toLowerCase();
+    const recipes = recipeCard.childNodes;
+    recipes.forEach(function(recipe){
+        if(recipe.className === 'card'){
+            let ustensiles = recipe.childNodes[2].childNodes[3].childNodes;
+            ustensiles.forEach(function(ustensile){
+                let ustensileName = ustensile.value.toLowerCase();
+                if(ustensileName.toLowerCase().includes(ustensileValue)){
+                    recipe.style.display = 'flex';
+                }else{
+                    recipe.style.display = 'none';
+                }
+            });
+        }
+    });
+}
 
