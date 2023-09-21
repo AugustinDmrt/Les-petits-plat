c31fd9f5b945fe986b2d4a545bf0dd08640461fb
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2023-09-21 16:52:25 +0200

    Ajout des options dans le selecteur

--- scripts/getData.js
+++ scripts/getData.js
@@ -12,7 +12,7 @@ function displayRecipes(recipes) {
         image.src = "../assets/images/RecipeImage/"+recipe.image;
         image.alt = "recette";
         const timeSpan = document.createElement("span");
-        timeSpan.textContent = recipe.time+" min";
+        timeSpan.textContent = recipe.time;
         cardImage.appendChild(image);
         cardImage.appendChild(timeSpan);
         card.appendChild(cardImage);
@@ -30,22 +30,8 @@ function displayRecipes(recipes) {
         descriptionHeader.textContent = "RECETTE";
         const description = document.createElement("p");
         description.textContent = recipe.description;
-        const appliance = document.createElement("input");
-        appliance.setAttribute("type", "hidden")
-        appliance.setAttribute("value",`${recipe.appliance}`);
         cardDescribe.appendChild(descriptionHeader);
         cardDescribe.appendChild(description);
-        cardDescribe.appendChild(appliance);
-        recipe.ustensils.forEach(ustensil => {
-            const listUstensils = document.createElement("div");
-            listUstensils.classList.add("listUstensil");
-            const inputUstensil = document.createElement("input");
-            inputUstensil.setAttribute("type", "hidden")
-            inputUstensil.setAttribute("value",`${ustensil}`);
-            
-            listUstensils.appendChild(inputUstensil);
-            cardDescribe.appendChild(listUstensils);
-        });
         card.appendChild(cardDescribe);
         
         const cardIngredient = document.createElement("div");
@@ -54,7 +40,6 @@ function displayRecipes(recipes) {
         ingredientsHeader.textContent = "INGREDIENT";
         const listComponent = document.createElement("div");
         listComponent.classList.add("listComponent");
-
         
         recipe.ingredients.forEach(ingredient => {
             const listItem = document.createElement("div");
@@ -62,17 +47,11 @@ function displayRecipes(recipes) {
             const ingredientName = document.createElement("p");
             ingredientName.textContent = ingredient.ingredient;
             const ingredientQuantity = document.createElement("span");
-            if (ingredient.quantity != undefined) {
-                ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit || ''}`;
-            } else {
-                ingredientQuantity.textContent = '';
-            }
+            ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit || ''}`;
             listItem.appendChild(ingredientName);
             listItem.appendChild(ingredientQuantity);
             listComponent.appendChild(listItem);
         });
-
-        
         
         cardIngredient.appendChild(ingredientsHeader);
         cardIngredient.appendChild(listComponent);
@@ -82,7 +61,56 @@ function displayRecipes(recipes) {
     nbrRecipe.innerHTML= recipes.length;
 }
 
+function fillSelector(datas){
+    const ingredientSelector = document.getElementById('ingredientFilter');
+    const applianceSelector = document.getElementById('appareilsFilter');
+    const ustensilsSelector = document.getElementById('ustensilesFilter');
+    
+    let appliances = [];
+    let ingredients = [];
+    let ustensils = [];
+
+    datas.forEach((data) => {
+        if (!appliances.includes(data.appliance)) {
+            appliances.push(data.appliance);
+        }
+        data.ingredients.forEach((ingr) => {
+            if (!ingredients.includes(ingr.ingredient)) {
+                ingredients.push(ingr.ingredient);
+            }
+        })
+        data.ustensils.forEach((ust) => {
+            if (!ustensils.includes(ust)) {
+                ustensils.push(ust);
+            }
+        })
+    })
+
+    appliances.forEach((data) => {
+        const optionAppliance = document.createElement('option') ;
+        optionAppliance.value = data;
+        optionAppliance.text = data;
+        applianceSelector.add(optionAppliance);
+    })
+    ingredients.forEach((data) => {
+        const optionIngredients = document.createElement('option') ;
+        optionIngredients.value = data;
+        optionIngredients.text = data;
+        ingredientSelector.add(optionIngredients);
+    })
+    ustensils.forEach((data) => {
+        const optionUstensil = document.createElement('option') ;
+        optionUstensil.value = data;
+        optionUstensil.text = data;
+        ustensilsSelector.add(optionUstensil);
+    })
+
+}
+
 fetch('../data/recipes.js')
 .then(response => response.json())
-.then(data => displayRecipes(data))
+.then(data => {
+    displayRecipes(data);
+    fillSelector(data);
+})
 .catch(err => console.log(err));
