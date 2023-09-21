783598eaad462630dab0552068e9a046b251734c
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2023-09-21 15:04:56 +0200

    Create js file for filter

---
+++ scripts/filterData.js
@@ -0,0 +1,11 @@
+let ingredientFilter = document.getElementById('ingredientFilter');
+let appareilsFilter = document.getElementById('appareilsFilter');
+let ustensilesFilter = document.getElementById('ustensilesFilter');
+let searchBar = document.getElementById('searchBar');
+let recipeCard = document.getElementById('recipList');
+
+
+
+
+searchBar.addEventListener('keyup', searchRecipes);
+
